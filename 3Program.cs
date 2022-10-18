96bd30a1cacd02ca21b3048b26637b930ef26574
Olqa01 <114251226+Olqa01@users.noreply.github.com>
2022-10-18 16:08:52 +0300

    Add files via upload

---
+++ 3Program.cs
@@ -0,0 +1,13 @@
+﻿// Task # 3. Задать номер четверти, показать диапазоны для возможных координат
+
+int number = new Random().Next(1, 5);
+string range = "";
+string num = "";
+
+if (number == 1) range = "x > 0, y > 0";
+else if (number == 2) range = "x < 0, y > ";
+else if (number == 3) range = "x < 0, y < 0";
+else range = "x > 0, y < 0";
+
+System.Console.WriteLine($"Для {number}-й четверти диапазон для возможных координат ( {range} )");
+System.Console.WriteLine();
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+ int dayNumber = new Random().Next(1, 8);
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string txtResult = "";
+
+if (dayNumber > 5) txtResult = $". это выходной день";
+else txtResult = $", это не выходной день.";
+
+System.Console.WriteLine();
+System.Console.WriteLine($"{dayNumber}-й день недели - это {Days[dayNumber - 1]}{txtResult}");
+System.Console.WriteLine();
